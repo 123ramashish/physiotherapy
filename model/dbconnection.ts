420f9595c81dbb54e7ca56b65e3645bd689df3fb
RAM ASHISH KUMAR <112244101+123ramashish@users.noreply.github.com>
2026-04-19 23:40:21 +0530

    Update dbconnection.ts

--- model/dbconnection.ts
+++ model/dbconnection.ts
@@ -9,7 +9,7 @@ if (!cached) {
     cached = (global as any).mongoose = { conn: null, promise: null };
 }
 
-export async function connectDB() {
+export async function connectDB() {   
     if (cached.conn) return cached.conn;
     if (!cached.promise) {
         cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => mongoose);
